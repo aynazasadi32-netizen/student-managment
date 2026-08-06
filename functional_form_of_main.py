8ee14d22c540961f2109a5fc6aea6dc42291db1a
aynazasadi <311685100+aynazasadi32-netizen@users.noreply.github.com>
2026-08-06 20:20:02 +0330

    fixed

--- functional_form_of_main.py
+++ functional_form_of_main.py
@@ -40,16 +40,6 @@ def save_data():
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
-def ending_the_code():
-   answer_input = input("do you want to back to the list of options?(y/n)")
-   if answer_input.upper() in ["YES", "BALE", "Y"]:
-        return True
-   else:
-    print("Good Bye😒")
-          
-
-
-
 
 
 #توابع اصلی
@@ -77,17 +67,29 @@ def add_student():
 
 def show_students():
     counter = 0
+    print("this list will be disapeard after 10 second")    
+    
     for student in Student_data:
         counter += 1
         print(f"{counter}: {student}")
-
+    for i in tqdm(range(10)):
+             time.sleep(1)
+    
 
 def delete_student():
-    show_students()   
-    choose = int(input("which student : "))     
-    delete = Student_data[choose-1]
-    Student_data.remove(delete)
-    save_data()
+    counter = 0    
+    for student in Student_data:
+        counter += 1
+        print(f"{counter}: {student}")  
+    try:
+        choose = int(input("which student : "))     
+        delete = Student_data[choose-1]
+        Student_data.remove(delete)
+        save_data()
+    except:
+        print("Invalid student number. try again after 3 second")
+        for i in tqdm(range(3)):
+         time.sleep(1)
 
 def process_choice(choice):
     if choice == 1:
@@ -102,6 +104,17 @@ def process_choice(choice):
     elif choice == 4:
         print("Good Bye😒")
         return False
+    else:
+        print("""
+============= Error 🚨==============
+
+Just enter a number between 1 and 4.
+
+Try again after 3 seconds.
+""")
+
+        for _ in tqdm(range(3)):
+            time.sleep(1)
 
     
 
@@ -110,13 +123,13 @@ def main():
         try:
             show_menu()
             choice = int(input("Enter your option: "))
-            process_choice(choice)
-            if not ending_the_code():
-             break
+            result = process_choice(choice)
+            
+            if result is False:
+                break
             clear_screen()
 
-
-        except (ValueError,IndexError):
+        except:
             print("""============= Error 🚨==============
                     
                        =====just enter a number between 1_4=====
